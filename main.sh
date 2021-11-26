f5e4b1e22bb70822d0c530ff2f790b220ad6783b
Frank Lan <frank.lan@aarnet.edu.au>
2021-11-26 18:06:20 +1100

    using namespace specified in yaml files

--- main.sh
+++ main.sh
@@ -5,11 +5,11 @@ minikube start
 BASE_DIR="$(realpath "$(dirname "$0")")"
 
 kubectl apply --filename "$BASE_DIR/mongo-namespace.yaml" --wait
-kubectl apply --filename "$BASE_DIR/mongo-secret.yaml" --namespace=mongo-stack --wait
-kubectl apply --filename "$BASE_DIR/mongo.yaml" --namespace=mongo-stack --wait
-kubectl apply --filename "$BASE_DIR/mongo-express-configmap.yaml" --namespace=mongo-stack --wait
-kubectl apply --filename "$BASE_DIR/mongo-express.yaml" --namespace=mongo-stack --wait
+kubectl apply --filename "$BASE_DIR/mongo-secret.yaml" --wait
+kubectl apply --filename "$BASE_DIR/mongo.yaml" --wait
+kubectl apply --filename "$BASE_DIR/mongo-express-configmap.yaml" --wait
+kubectl apply --filename "$BASE_DIR/mongo-express.yaml" --wait
 
 kubectl get all --namespace=mongo-stack | grep mongo
 
-minikube service mongo-express-service
+minikube service --namespace=mongo-stack mongo-express-service
